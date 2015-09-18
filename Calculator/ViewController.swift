dbd953cfa85a1ff848a4c5b4db76f04035a18fb5
Sam Jones <bsamjones@gmail.com>
2015-09-18 13:47:05 -0500

    first commit of baby's first calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTypingANumber: Bool = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func append_digit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTypingANumber {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit //get rid of the zero
+            userIsTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
     }
-
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTypingANumber = false
+        }
+    }
 
 }
 
